052bf2a28a9b72406559e61d943a395c6e92427a
ehm <emrahhalacoglu@gmail.com>
2022-10-31 14:37:34 +0300

    fixed

--- Code.java
+++ Code.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 public class Code {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        int yil;
-        
-        System.out.print("Artik yil sorgulamak istediginiz yili giriniz : ");
-        yil=input.nextInt();
+        try (Scanner input = new Scanner(System.in)) {
+            int yil;
+            
+            System.out.print("Artik yil sorgulamak istediginiz yili giriniz : ");
+            yil=input.nextInt();
 
-        if(yil>=0&&(yil%4==0)){
-            if(yil%100==0&&yil%400!=0){
-                System.out.println(yil+" Bir Artik Yil Degildir. ");}
+            if(yil>=0&&(yil%4==0)){
+                if(yil%100==0&&yil%400!=0){
+                    System.out.println(yil+" Bir Artik Yil Degildir. ");}
+                else{
+                    System.out.println(yil+" Bir Artik Yildir.");}}
             else{
-                System.out.println(yil+" Bir Artik Yildir.");}}
-        else{
-            System.out.println(yil+" Bir Artik Yil Degildir. ");}
+                System.out.println(yil+" Bir Artik Yil Degildir. ");}
+        }
     }
     
 }
